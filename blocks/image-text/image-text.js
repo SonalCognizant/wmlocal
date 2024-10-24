21a684ee09231619004f29570282ba74105f3a61
G <2035448@cognizant.com>
2024-10-24 19:04:44 +0530

    image-text class name add

--- blocks/image-text/image-text.js
+++ blocks/image-text/image-text.js
@@ -1,16 +1,16 @@
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
-  block.classList.add(`columns-${cols.length}-cols`);
+  block.classList.add(`image-text-${cols.length}-cols`);
 
-  // setup image columns
+  // setup image image-text
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
       const pic = col.querySelector('picture');
       if (pic) {
         const picWrapper = pic.closest('div');
         if (picWrapper && picWrapper.children.length === 1) {
-          // picture is only content in column
-          picWrapper.classList.add('columns-img-col');
+          // picture is only content in image-text
+          picWrapper.classList.add('image-text-img-col');
         }
       }
     });
