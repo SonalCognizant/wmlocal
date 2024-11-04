4c41d2f6685a99638ceb62e126738c11b22dd166
Tyagi <2358108@cognizant.com>
2024-11-04 14:44:11 +0530

    Adding promo-image block js changes

--- blocks/promo-image/promo-image.js
+++ blocks/promo-image/promo-image.js
@@ -1,21 +1,21 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 export default function decorate(block) {
   /* change to ul, li */
-  console.log(block.children[0].innerHTML,"block");
+//   console.log(block.children[0].innerHTML,"block");
   let backgroundImage = block.children[0].innerText;
   let title = block.children[1].innerText;
   console.log(backgroundImage,title)
-//   const ul = document.createElement('ul');
-//   [...block.children].forEach((row) => {
-//     const li = document.createElement('li');
-//     while (row.firstElementChild) li.append(row.firstElementChild);
-//     [...li.children].forEach((div) => {
-//       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-//       else div.className = 'cards-card-body';
-//     });
-//     ul.append(li);
-//   });
-//   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-//   block.textContent = '';
-//   block.append(ul);
+  const ul = document.createElement('ul');
+  [...block.children[2]].forEach((row) => {
+    const li = document.createElement('li');
+    while (row.firstElementChild) li.append(row.firstElementChild);
+    [...li.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      else div.className = 'cards-card-body';
+    });
+    ul.append(li);
+  });
+  ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  block.textContent = '';
+  block.append(ul);
 }
