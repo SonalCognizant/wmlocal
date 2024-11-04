7cd19cddea45c6297c406118ddbec93272d7bd30
Tyagi <2358108@cognizant.com>
2024-11-04 16:50:07 +0530

    Adding promo-image block js changes

--- blocks/promo-image/promo-image.js
+++ blocks/promo-image/promo-image.js
@@ -11,6 +11,8 @@ export default function decorate(block) {
   imageWrapperDiv.style.backgroundSize = "cover";
   imageWrapperDiv.style.backgroundPosition = "center";
   imageWrapperDiv.style.backgroundRepeat = "no-repeat";
+  imageWrapperDiv.style.textAlign = "center";
+  imageWrapperDiv.innerHTML = `<h3>${title}</h3>`;
     [...block.children].forEach((row) => {
     const li = document.createElement('li');
     console.log(row.firstElementChild,"row")
