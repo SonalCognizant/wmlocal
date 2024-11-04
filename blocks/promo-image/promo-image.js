5541c2b7b4dcc2bdc8e614b0c30712e051db8027
Tyagi <2358108@cognizant.com>
2024-11-04 09:30:41 +0530

    Adding promo-image block js changes

--- blocks/promo-image/promo-image.js
+++ blocks/promo-image/promo-image.js
@@ -19,8 +19,39 @@ export default function decorate(block) {
   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(ul);
+  decorateExternalImages(main, '//External Image//');
+  
+  // decorate external images with implicit external image marker
+  decorateExternalImages(main);
 }
 // second way of adding external image is through 
 // export function decorateMain(main) {
 //     // decorate external images with explicit external image marker
 //     decorateExternalImages(main, '//External Image//');
+function decorateExternalImages(ele, deliveryMarker) {
+    const extImages = ele.querySelectorAll('a');
+    extImages.forEach((extImage) => {
+      if (isExternalImage(extImage, deliveryMarker)) {
+        const extImageSrc = extImage.getAttribute('href');
+        const extPicture = createOptimizedPicture(extImageSrc);
+  
+        /* copy query params from link to img */
+        const extImageUrl = new URL(extImageSrc);
+        const { searchParams } = extImageUrl;
+        extPicture.querySelectorAll('source, img').forEach((child) => {
+          if (child.tagName === 'SOURCE') {
+            const srcset = child.getAttribute('srcset');
+            if (srcset) {
+              child.setAttribute('srcset', appendQueryParams(new URL(srcset, extImageSrc), searchParams));
+            }
+          } else if (child.tagName === 'IMG') {
+            const src = child.getAttribute('src');
+            if (src) {
+              child.setAttribute('src', appendQueryParams(new URL(src, extImageSrc), searchParams));
+            }
+          }
+        });
+        extImage.parentNode.replaceChild(extPicture, extImage);
+      }
+    });
+  }
