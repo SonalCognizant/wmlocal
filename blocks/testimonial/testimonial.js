6c9c9f2ca5750253bf18efc22be9388b87310a8a
P <2198236@cognizant.com>
2024-11-05 13:14:02 +0530

    adding link

--- blocks/testimonial/testimonial.js
+++ blocks/testimonial/testimonial.js
@@ -6,12 +6,12 @@ export default function decorate(block) {
   [...block.children].forEach((row, index) => {
     if (index > 0){
       const li = document.createElement('li');
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
-    ul.append(li);
+      while (row.firstElementChild) li.append(row.firstElementChild);
+      [...li.children].forEach((div) => {
+        if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+        else div.className = 'cards-card-body';
+      });
+      ul.append(li);
     }
   });
   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
@@ -20,7 +20,11 @@ export default function decorate(block) {
   const imageWrapperDiv = block.parentElement;
   let title = block.children[0].innerText;
   const mainHeading = document.createElement("h3");
-  mainHeading.classList.add("promo-image-heading");
+  mainHeading.classList.add("testimonial-heading");
   mainHeading.textContent = `${title}`;
   imageWrapperDiv.prepend(mainHeading);
+  const hyperLink = document.createElement("a");
+  hyperLink.classList.add("testimonial-link");
+  hyperLink.textContent = `${link}`;
+  imageWrapperDiv.append(hyperLink);
 }
