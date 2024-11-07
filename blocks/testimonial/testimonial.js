2ae9eef99796e0f930fc5eaa45d2b1d98b75746f
P <2198236@cognizant.com>
2024-11-07 09:59:27 +0530

    js

--- blocks/testimonial/testimonial.js
+++ blocks/testimonial/testimonial.js
@@ -5,33 +5,33 @@ export default function decorate(block) {
   const parentElement = block.parentElement;
   const ul = document.createElement('ul');
   let title = block.children[0].children[0].children[0].innerText;
+  let title1 = block.children[0].innerText;
   console.log(title,"title");
   console.log(block.children[0].innerText,"innerText");
   //prepend the title
   const mainHeading = document.createElement("h2");
   mainHeading.classList.add("testimonial-heading");
-  mainHeading.textContent = `${title}`;
+  mainHeading.textContent = `${title1}`;
   parentElement.prepend(mainHeading);
   //append the link
-//   let link = block.children[block.children.length - 1].innerText;
-//   const hyperLink = document.createElement("a");
-//   hyperLink.classList.add("testimonial-link");
-//   hyperLink.setAttribute("href","#");
-//   hyperLink.textContent = `${link}`;
-//   parentElement.append(hyperLink);
-//   [...block.children].forEach((row, index) => {
-//     if (index < block.children.length-1 && index > 0) {
-//       const li = document.createElement('li');
-//       while (row.firstElementChild) li.append(row.firstElementChild);
-//       [...li.children].forEach((div) => {
-//         if (div.children.length === 1 && div.querySelector('picture')) div.className = 'testimonial-image';
-//         else div.className = 'testimonial-card';
-//       });
-//       ul.append(li);
-//     }
-//   });
-//   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-//   block.textContent = '';
-//   block.append(ul);
-  
+  let link = block.children[block.children.length - 1].innerText;
+  const hyperLink = document.createElement("a");
+  hyperLink.classList.add("testimonial-link");
+  hyperLink.setAttribute("href","#");
+  hyperLink.textContent = `${link}`;
+  parentElement.append(hyperLink);
+  [...block.children].forEach((row, index) => {
+    if (index < block.children.length-1 && index > 0) {
+      const li = document.createElement('li');
+      while (row.firstElementChild) li.append(row.firstElementChild);
+      [...li.children].forEach((div) => {
+        if (div.children.length === 1 && div.querySelector('picture')) div.className = 'testimonial-image';
+        else div.className = 'testimonial-card';
+      });
+      ul.append(li);
+    }
+  });
+  ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  block.textContent = '';
+  block.append(ul);
 }
