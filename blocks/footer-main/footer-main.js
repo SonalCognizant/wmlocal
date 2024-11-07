d7ad95f5b4ac810ad41ed3ba10d11305f2a4f5ec
Tyagi <2358108@cognizant.com>
2024-11-07 17:02:41 +0530

    writing js for footer

--- blocks/footer-main/footer-main.js
+++ blocks/footer-main/footer-main.js
@@ -1,11 +1,7 @@
 import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
 import {logo} from "../../images/footer-main/WellmarkLogo.png"
-/**
- * loads and decorates the footer
- * @param {Element} block The footer block element
- */
-export default async function decorate(block) {
+export default function decorate(block) {
   console.log(block,"block")
   console.log(logo,"logo");
   const footerMain= block.children[1].classList.add("contact-div");
