67b452583a680346765870ee815816879ee0bc6c
Tyagi <2358108@cognizant.com>
2024-11-07 10:35:38 +0530

    changes for footer

--- blocks/footer-main/footer-main.js
+++ blocks/footer-main/footer-main.js
@@ -5,12 +5,17 @@ import { loadFragment } from '../fragment/fragment.js';
  * loads and decorates the footer
  * @param {Element} block The footer block element
  */
-export default function decorate(block) {
+export default async function decorate(block) {
   // load footer as fragment
   console.log(block);
-  [...block.children].forEach((row, index) => {
-      const li = document.createElement("li");
-      while (row.firstElementChild) li.append(row.firstElementChild);
-      ul.append(li);
-  });
+  const footerMeta = getMetadata('footer');
+  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
+  const fragment = await loadFragment(footerPath);
+
+  // decorate footer DOM
+  block.textContent = '';
+  const footer = document.createElement('div');
+  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+
+  block.append(footer);
 }
