a02403c231196d3e3fc91aed322e221037056b08
Tyagi <2358108@cognizant.com>
2024-11-07 11:52:44 +0530

    changes for footer

--- blocks/footer-main/footer-main.js
+++ blocks/footer-main/footer-main.js
@@ -7,15 +7,20 @@ import { loadFragment } from '../fragment/fragment.js';
  */
 export default async function decorate(block) {
   // load footer as fragment
-  console.log(block);
-  const footerMeta = getMetadata('footer');
-  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
-  const fragment = await loadFragment(footerPath);
+  // console.log(block);
+  // [...block.children].forEach((row, index) => {
+  //     const li = document.createElement("li");
+  //     while (row.firstElementChild) li.append(row.firstElementChild);
+  //     ul.append(li);
+  // });
+  // const footerMeta = getMetadata('footer');
+  // const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
+  // const fragment = await loadFragment(footerPath);
 
-  // decorate footer DOM
-  block.textContent = '';
-  const footer = document.createElement('div');
-  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+  // // decorate footer DOM
+  // block.textContent = '';
+  // const footer = document.createElement('div');
+  // while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
-  block.append(footer);
+  return block
 }
