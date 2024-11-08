a667f055f06242bb3b99aed13471a15e9f281238
Bhargava <2363065@cognizant.com>
2024-11-08 11:59:28 +0530

    added megamenu

--- blocks/megamenu/megamenu.js
+++ blocks/megamenu/megamenu.js
@@ -1,5 +1,4 @@
-import { getMetadata } from '../../scripts/aem.js';
-import { loadFragment } from '../fragment/fragment.js';
+//const menuLinks=[];
 
 function renderMegaMenu() {
   return '<ul><li>Hello Hello</li></ul>';
@@ -10,15 +9,9 @@ function renderMegaMenu() {
  * @param {Element} block The header block element
  */
 export default async function decorate(block) {
-  // load nav as fragment
-  const navMeta = getMetadata('nav');
-  const navPath = navMeta ? new URL(navMeta, window.location).pathname : '/nav';
-  const fragment = await loadFragment(navPath);
-  // decorate nav DOM
   block.textContent = '';
   const nav = document.createElement('nav');
   nav.id = 'nav';
-  while (fragment.firstElementChild) nav.append(fragment.firstElementChild);
   nav.append(renderMegaMenu());
   const navWrapper = document.createElement('div');
   navWrapper.className = 'nav-wrapper';
