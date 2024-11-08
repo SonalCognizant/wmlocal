1a973568f3079f374f1000cfebee5e5caa8e91da
Bhargava <2363065@cognizant.com>
2024-11-08 12:11:24 +0530

    added megamenu

--- blocks/megamenu/megamenu.js
+++ blocks/megamenu/megamenu.js
@@ -1,7 +1,14 @@
 //const menuLinks=[];
 
-function renderMegaMenu() {
-  return '<ul><li>Hello Hello</li></ul>';
+function renderMegaMenu(nav) {
+  const section1 = document.createElement('div');
+  section1.className = 'sec1';
+  const section2 = document.createElement('div');
+  section2.className = 'sec2';
+  section2.createElement('ul');
+  //loop through menuLinks json to create li
+  nav.append(section1);
+  nav.append(section1);
 }
 
 /**
@@ -12,7 +19,7 @@ export default async function decorate(block) {
   block.textContent = '';
   const nav = document.createElement('nav');
   nav.id = 'nav';
-  nav.append(renderMegaMenu());
+  renderMegaMenu(nav);
   const navWrapper = document.createElement('div');
   navWrapper.className = 'nav-wrapper';
   navWrapper.append(nav);
