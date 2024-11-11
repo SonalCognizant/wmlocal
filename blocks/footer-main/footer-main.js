e4744b84a791fb3744942e8609b47fce09a1fef5
Tyagi <2358108@cognizant.com>
2024-11-11 14:54:06 +0530

    writing js for footer

--- blocks/footer-main/footer-main.js
+++ blocks/footer-main/footer-main.js
@@ -13,62 +13,8 @@ export default function decorate(block) {
   logoDiv.prepend(imgDiv);
   logoDiv.classList.add("logo-social-div");
   const socialDiv = block.children[0].children[1];
-  // const linkImg1 = document.createElement("img");
-  // const linkImg2 = document.createElement("img");
-  // const linkImg3 = document.createElement("img");
-  // const linkImg4 = document.createElement("img");
-  // const linkImg5 = document.createElement("img");
-  // const anchor1 = document.createElement("a");
-  // const anchor2 = document.createElement("a");
-  // const anchor3 = document.createElement("a");
-  // const anchor4 = document.createElement("a");
-  // const anchor5 = document.createElement("a");
-  // anchor1.setAttribute("href", "#");
-  // anchor2.setAttribute("href", "#");
-  // anchor3.setAttribute("href", "#");
-  // anchor4.setAttribute("href", "#");
-  // anchor5.setAttribute("href", "#");
-  // linkImg1.setAttribute("data-icon-name", "right-arrow");
-  // linkImg1.src = "/icons/facebook.svg";
-  // linkImg1.setAttribute("loading", "eager");
-  // linkImg1.className = "link-img";
-
-  // linkImg2.src = "/icons/x.svg";
-  // linkImg2.setAttribute("data-icon-name", "right-arrow");
-  // linkImg2.setAttribute("loading", "eager");
-  // linkImg2.className = "link-img";
-
-  // linkImg3.src = "/icons/youtube.svg";
-  // linkImg3.setAttribute("data-icon-name", "right-arrow");
-  // linkImg3.setAttribute("loading", "eager");
-  // linkImg3.className = "link-img";
-
-  // linkImg4.src = "/icons/instagram.svg";
-  // linkImg4.setAttribute("data-icon-name", "right-arrow");
-  // linkImg4.setAttribute("loading", "eager");
-  // linkImg4.className = "link-img";
-
-  // linkImg5.src = "/icons/linkedin.svg";
-  // linkImg5.setAttribute("data-icon-name", "right-arrow");
-  // linkImg5.setAttribute("loading", "eager");
-  // linkImg5.className = "link-img";
-
-  // anchor1.append(linkImg1);
-  // anchor2.append(linkImg2);
-  // anchor3.append(linkImg3);
-  // anchor4.append(linkImg4);
-  // anchor5.append(linkImg5);
-  // const anchor6 = anchor1.cloneNode(true)
-  // const anchor7 = anchor2.cloneNode(true)
-  // const anchor8 = anchor3.cloneNode(true)
-  // const anchor9 = anchor4.cloneNode(true)
-  // const anchor10 = anchor5.cloneNode(true)
-
   socialDiv.classList.add("social-div");
-  // socialDiv.append(anchor1, anchor2, anchor3, anchor4, anchor5);
-  const socialDiv2=document.createElement('div')
-  socialDiv2.append(anchor6, anchor7, anchor8, anchor9, anchor10);
-  socialDiv2.classList.add("social-div-mobile");
+  const socialDiv2 = socialDiv.cloneNode(true);
   const attentionDiv = block.children[4];
   attentionDiv.append(socialDiv2);
 }
