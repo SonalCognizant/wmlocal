c5847baaf715ca9b93f84703c60f80f2cf6068c8
Tyagi <2358108@cognizant.com>
2024-11-12 19:31:14 +0530

    js for footers

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -8,30 +8,30 @@ import { loadFragment } from "../fragment/fragment.js";
 export default async function decorate(block) {
   // load footer as fragment
   const footerMeta = getMetadata("footer");
-  let footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
-  console.log(footerPath,"footerPath")
-  // let footerPath;
-  // if (window.location.pathname === "/providers/*") {
-  //   footerPath = "/providers/provider";
-  // } else if (window.location.pathname === "/shop/shopplans/*") {
-  //   footerPath = "/shop/shopplans/medicareadvantage";
-  // } else {
-  //   footerPath = "/footer";
-  // }
-  // if (window.location.pathname.includes("/providers/")) {
-  //   footerPath = "/providers/provider";
-  // } else if (window.location.pathname.includes("/shop/shopplans/")) {
-  //   footerPath = "/shop/shopplans/medicareadvantage";
-  // } else {
-  //   footerPath = "/footer";
-  // }
+  let footerPath = footerMeta
+    ? new URL(footerMeta, window.location).pathname
+    : "/footer";
+  console.log(footerPath, "footerPath");
+  if (window.location.pathname.includes("/providers/")) {
+    footerPath = "/providers/provider";
+  } else if (window.location.pathname.includes("/shop/shopplans/")) {
+    footerPath = "/shop/shopplans/medicareadvantage";
+  } else {
+    footerPath = "/footer";
+  }
   const fragment = await loadFragment(footerPath);
-  console.log(fragment,"fragment")
+  console.log(fragment, "fragment");
 
   block.textContent = "";
   const footer = document.createElement("div");
-  console.log(fragment.firstElementChild,"fragmentfirstChild")
-  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+  console.log(fragment.firstElementChild, "fragmentfirstChild");
+  while (fragment.firstElementChild) {
+    const footerMain = document.querySelector('.footer-main-wrapper')
+    const footerProvider = document.querySelector('.footer-provider-wrapper')
+    const footerMedAdv = document.querySelector('.footer-medadv-wrapper')
+    console.log(footerMain,footerProvider,footerMedAdv)
+    footer.append(fragment.firstElementChild);
+  }
 
   block.append(footer);
 }
