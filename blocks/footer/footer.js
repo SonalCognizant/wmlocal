0adc029d46140acaf73bd73c4af3dd362c075ea4
Tyagi <2358108@cognizant.com>
2024-11-12 18:49:43 +0530

    js for footers

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -8,8 +8,9 @@ import { loadFragment } from "../fragment/fragment.js";
 export default async function decorate(block) {
   // load footer as fragment
   const footerMeta = getMetadata("footer");
-  // let footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
-  let footerPath;
+  let footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
+  console.log(footerPath)
+  // let footerPath;
   // if (window.location.pathname === "/providers/*") {
   //   footerPath = "/providers/provider";
   // } else if (window.location.pathname === "/shop/shopplans/*") {
@@ -17,17 +18,19 @@ export default async function decorate(block) {
   // } else {
   //   footerPath = "/footer";
   // }
-  if (window.location.pathname.includes("/providers/")) {
-    footerPath = "/providers/provider";
-  } else if (window.location.pathname.includes("/shop/shopplans/*")) {
-    footerPath = "/shop/shopplans/medicareadvantage";
-  } else {
-    footerPath = "/footer";
-  }
+  // if (window.location.pathname.includes("/providers/")) {
+  //   footerPath = "/providers/provider";
+  // } else if (window.location.pathname.includes("/shop/shopplans/")) {
+  //   footerPath = "/shop/shopplans/medicareadvantage";
+  // } else {
+  //   footerPath = "/footer";
+  // }
   const fragment = await loadFragment(footerPath);
+  console.log(fragment)
 
   block.textContent = "";
   const footer = document.createElement("div");
+  console.log(fragment.firstElementChild)
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
   block.append(footer);
