611c00a1ce79cc37a8c34e8ca67c782146c5cd66
Tyagi <2358108@cognizant.com>
2024-11-12 22:15:16 +0530

    js for footers

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -6,14 +6,7 @@ import { loadFragment } from "../fragment/fragment.js";
  * @param {Element} block The footer block element
  */
 export default async function decorate(block) {
-  // load footer as fragment
-  // const footerMeta = getMetadata("footer");
-  // let footerPath = footerMeta
-  //   ? new URL(footerMeta, window.location).pathname
-  //   : "/footer";
-  // const fragment = await loadFragment(footerPath);
   block.textContent = "";
-  // const footer = document.createElement("div");
   const footerMain = document.querySelector(".footer-main-wrapper");
   const footerProvider = document.querySelector(".footer-provider-wrapper");
   const footerMedAdv = document.querySelector(".footer-medadv-wrapper");
@@ -24,5 +17,4 @@ export default async function decorate(block) {
   } else {
     block.append(footerMain)
   }
-//  block.append(footer);
 }
