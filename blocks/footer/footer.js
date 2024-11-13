6041fdab9fa270a7ab9538878f97153e0c6e4528
Tyagi <2358108@cognizant.com>
2024-11-13 12:16:18 +0530

    js for footers

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,38 +1,3 @@
-import { getMetadata } from "../../scripts/aem.js";
-import { loadFragment } from "../fragment/fragment.js";
-/**
- * loads and decorates the footer
- * @param {Element} block The footer block element
- */
-// export default async function decorate(block) {
-//   block.textContent = "";
-//   const footerMeta = getMetadata('footer');
-//   console.log(footerMeta)
-//   const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
-//   console.log(footerPath)
-//   const fragment = await loadFragment(footerPath);
-//   console.log(fragment)
-//   const footer = document.createElement("div");
-//   const footerMain = document.querySelector(".footer-main-wrapper");
-//   // console.log(footerMain,"footer-main");
-//   const footerProvider = document.querySelector(".footer-provider-wrapper");
-//   // console.log(footerProvider,"footer-provider");
-//   const footerMedAdv = document.querySelector(".footer-medadv-wrapper");
-//   console.log(footerMedAdv,"footer-medAdv");
-//   if (window.location.pathname.includes("/providers/")) {
-//     footer.append(footerProvider);
-//   } else if (window.location.pathname.includes("/shop/")) {
-//     footer.append(footerMedAdv);
-//   } else {
-//     console.log(footerMain);
-//     footer.append(footerMain);
-//   }
-//   block.append(footer);
-// }
-
-// import { getMetadata } from '../../scripts/aem.js';
-// import { loadFragment } from '../fragment/fragment.js';
-
 /**
  * loads and decorates the footer
  * @param {Element} block The footer block element
