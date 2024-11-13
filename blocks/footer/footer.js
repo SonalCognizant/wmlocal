775d87a548228c00ec01bd2ee9a9d4f495d2de95
Tyagi <2358108@cognizant.com>
2024-11-13 10:36:43 +0530

    js for footers

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -3,10 +3,10 @@
  * @param {Element} block The footer block element
  */
 export default async function decorate(block) {
-  block.textContent = "";
-  const footerMeta = getMetadata('footer');
-  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
-  const fragment = await loadFragment(footerPath);
+  // block.textContent = "";
+  // const footerMeta = getMetadata('footer');
+  // const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
+  // const fragment = await loadFragment(footerPath);
   const footer = document.createElement("div");
   const footerMain = document.querySelector(".footer-main-wrapper");
   const footerProvider = document.querySelector(".footer-provider-wrapper");
