1bc7c5b05c3c799e57cfdd128d292b4832a3827a
Tyagi <2358108@cognizant.com>
2024-11-13 10:29:46 +0530

    js for footers

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -4,6 +4,9 @@
  */
 export default async function decorate(block) {
   block.textContent = "";
+  const footerMeta = getMetadata('footer');
+  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
+  const fragment = await loadFragment(footerPath);
   const footer = document.createElement("div");
   const footerMain = document.querySelector(".footer-main-wrapper");
   const footerProvider = document.querySelector(".footer-provider-wrapper");
@@ -17,3 +20,23 @@ export default async function decorate(block) {
   }
   block.append(footer);
 }
+// import { getMetadata } from '../../scripts/aem.js';
+// import { loadFragment } from '../fragment/fragment.js';
+ 
+// /**
+//  * loads and decorates the footer
+//  * @param {Element} block The footer block element
+//  */
+// export default async function decorate(block) {
+//   // load footer as fragment
+//   const footerMeta = getMetadata('footer');
+//   const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
+//   const fragment = await loadFragment(footerPath);
+ 
+//   // decorate footer DOM
+//   block.textContent = '';
+//   const footer = document.createElement('div');
+//   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+ 
+//   block.append(footer);
+// }
