14919ed3200ab0a6da61bc7ee92a88ff84a2bc95
Tyagi <2358108@cognizant.com>
2024-11-13 10:44:59 +0530

    js for footers

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,3 +1,5 @@
+import { getMetadata } from '../../scripts/aem.js';
+import { loadFragment } from '../fragment/fragment.js';
 /**
  * loads and decorates the footer
  * @param {Element} block The footer block element
