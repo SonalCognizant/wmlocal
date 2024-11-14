017dfd02b40d9011c061489508955d15ae1ffebd
Tyagi <2358108@cognizant.com>
2024-11-14 10:27:21 +0530

    es lint

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -4,24 +4,25 @@
  */
 import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
+
 export default async function decorate(block) {
   // load footer as fragment
-  const footerMeta = getMetadata("footer");
+  const footerMeta = getMetadata('footer');
   const footerPath = footerMeta
     ? new URL(footerMeta, window.location).pathname
-    : "/footer";
+    : '/footer';
   const fragment = await loadFragment(footerPath);
 
   // decorate footer DOM
   block.textContent = "";
-  const footer = document.createElement("div");
+  const footer = document.createElement('div');
   if (fragment.firstElementChild) {
-    const footerMain = fragment.firstElementChild.children[0]
+    const footerMain = fragment.firstElementChild.children[0];
     const footerProvider = fragment.firstElementChild.children[1];
     const footerMedAdv = fragment.firstElementChild.children[2];
-    if (window.location.pathname.includes("/providers/")) {
+    if (window.location.pathname.includes('/providers/')) {
       footer.append(footerProvider);
-    } else if (window.location.pathname.includes("/shop/")) {
+    } else if (window.location.pathname.includes('/shop/')) {
       footer.append(footerMedAdv);
     } else {
       footer.append(footerMain);
