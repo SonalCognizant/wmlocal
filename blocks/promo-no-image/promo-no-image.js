3cd0ffd6cdf6ea737e0299860043ff1d93cd4882
Tyagi <2358108@cognizant.com>
2024-11-14 10:13:36 +0530

    es lint

--- blocks/promo-no-image/promo-no-image.js
+++ blocks/promo-no-image/promo-no-image.js
@@ -1,32 +1,32 @@
-import { createOptimizedPicture } from "../../scripts/aem.js";
+import { createOptimizedPicture } from '../../scripts/aem.js';
 export default function decorate(block) {
   /* change to ul, li */
   let title = block.children[0].innerText;
-  const ul = document.createElement("ul");
+  const ul = document.createElement('ul');
   const imageWrapperDiv = block.parentElement;
-  const mainHeading = document.createElement("h3");
-  mainHeading.classList.add("promo-no-image-heading");
+  const mainHeading = document.createElement('h3');
+  mainHeading.classList.add('promo-no-image-heading');
   mainHeading.textContent = `${title}`;
   imageWrapperDiv.prepend(mainHeading);
   [...block.children].forEach((row, index) => {
     if (index > 0) {
-      const li = document.createElement("li");
+      const li = document.createElement('li');
       while (row.firstElementChild) li.append(row.firstElementChild);
       [...li.children].forEach((div) => {
-        if (div.children.length === 1 && div.querySelector("picture"))
-          div.className = "cards-card-image";
-        else div.className = "cards-card-body";
+        if (div.children.length === 1 && div.querySelector('picture'))
+          div.className = 'cards-card-image';
+        else div.className = 'cards-card-body';
       });
       ul.append(li);
     }
   });
-  ul.querySelectorAll("picture > img").forEach((img) =>
+  ul.querySelectorAll('picture > img').forEach((img) =>
     img
       .closest("picture")
       .replaceWith(
-        createOptimizedPicture(img.src, img.alt, false, [{ width: "750" }])
+        createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])
       )
   );
-  block.textContent = "";
+  block.textContent = '';
   block.append(ul);
 }
