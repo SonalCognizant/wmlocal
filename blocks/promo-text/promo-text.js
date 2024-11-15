92b18c8aebb335ab8e70738e0aa62219e39aa65d
Tyagi <2358108@cognizant.com>
2024-11-16 04:39:01 +0530

    promo text changes

--- blocks/promo-text/promo-text.js
+++ blocks/promo-text/promo-text.js
@@ -2,11 +2,14 @@ export default function decorate(block) {
   const title = block.children[0].textContent;
   const description = block.children[1].textContent;
   const button = block.children[2].children[0].children[0];
+  button.classList.add('button-container');
   console.log(title, description, button);
   const div = document.createElement('div');
   const heading = document.createElement('h2');
   heading.append(title);
+  heading.classList.add('title');
   const paragraph = document.createElement('p');
+  paragraph.classList.add('description');
   paragraph.append(description);
   div.append(heading, paragraph, button);
   console.log(div);
