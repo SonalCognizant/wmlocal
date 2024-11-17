129670a118b011d84d4818f62eb742f5bab51807
Tyagi <2358108@cognizant.com>
2024-11-17 14:28:30 +0530

    promo text changes

--- blocks/promo-text-sm-icon/promo-text-sm-icon.js
+++ blocks/promo-text-sm-icon/promo-text-sm-icon.js
@@ -2,7 +2,7 @@ export default function decorate(block) {
   const svgIcon = block.children[0].children[0].children[0];
   const title = block.children[0].textContent;
   const description = block.children[1].textContent;
-  const button = block.children[2].children[0].children[0];
+  const button = block.children[2].children[0].children[0].children[0].children[0];
   button.classList.add('button', 'primary');
   const div = document.createElement('div');
   const div2 = document.createElement('div');
