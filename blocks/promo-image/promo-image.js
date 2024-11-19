4deeb3611e9d37930ce92a82dad8f12f301eeae3
Tyagi <2358108@cognizant.com>
2024-11-19 12:29:05 +0530

    promo-image issues

--- blocks/promo-image/promo-image.js
+++ blocks/promo-image/promo-image.js
@@ -15,18 +15,7 @@ export default function decorate(block) {
   mainHeading.textContent = `${title}`;
   imageWrapperDiv.prepend(mainHeading);
   const promoClass = `promo-${block.children.length - 2}-column`;
-  console.log(promoClass)
   ul.classList.add(promoClass);
-  // const blockDiv = document.querySelector('.promo-image ul');
-  // console.log(blockDiv);
-  // if (block.children.length === 6) {
-  //   console.log('promo image 4 column');
-  //   blockDiv.classList.add('4-column');
-  // } else if (block.children.length === 5) {
-  //   console.log('promo image 3 column');
-  // } else if (block.children.length === 4) {
-  //   console.log('promo image 2 column');
-  // }
   [...block.children].forEach((row, index) => {
     if (index > 1) {
       const li = document.createElement('li');
@@ -35,9 +24,7 @@ export default function decorate(block) {
         if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
         else div.className = 'cards-card-body';
       });
-      // console.log(li.length,"li");
       ul.append(li);
-      // console.log(ul.length,"ul")
     }
   });
   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
