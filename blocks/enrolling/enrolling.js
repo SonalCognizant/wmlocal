227c1bb5a2f99614c426bc67650de7e63ba57fe0
Tyagi <2358108@cognizant.com>
2024-11-22 17:56:10 +0530

    promo-no-image issues

--- blocks/enrolling/enrolling.js
+++ blocks/enrolling/enrolling.js
@@ -1,15 +1,7 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-
 export default function decorate(block) {
-  /* change to ul, li */
-  // const backgroundImage = block.children[0].innerText;
   const title = block.children[0].innerText;
   const ul = document.createElement('ul');
   const imageWrapperDiv = block.parentElement;
-  // imageWrapperDiv.style.backgroundImage = `url(${backgroundImage})`;
-  // imageWrapperDiv.style.backgroundSize = 'cover';
-  // imageWrapperDiv.style.backgroundPosition = 'center';
-  // imageWrapperDiv.style.backgroundRepeat = 'no-repeat';
   const mainHeading = document.createElement('h3');
   mainHeading.classList.add('enrolling-heading');
   mainHeading.textContent = `${title}`;
@@ -17,14 +9,12 @@ export default function decorate(block) {
   [...block.children].forEach((row, index) => {
     if (index > 0) {
       const li = document.createElement('li');
-      console.log(row.firstElementChild, 'hi');
-      console.log(row);
+      let count = 0;
+      li.classList.add(`${count}-column`);
+      count += 1;
+      console.log(count, 'count');
       while (row.firstElementChild) {
-        let count = 0;
         li.append(row.firstElementChild);
-        li.classList.add(`${count}-column`);
-        count += 1;
-        console.log(count, 'count');
         [...li.children].forEach((div) => {
           div.className = 'cards-card-body';
         });
