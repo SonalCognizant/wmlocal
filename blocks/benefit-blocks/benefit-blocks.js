c64153c20f8e37f1f8ce5b751be53ba426bf0a4a
Muthu Soundara Pandian <soundhar.sms1992@gmail.com>
2024-11-24 23:01:24 +0530

    benefit-blocks-fixes

--- blocks/benefit-blocks/benefit-blocks.js
+++ blocks/benefit-blocks/benefit-blocks.js
@@ -1,41 +1,22 @@
 export default function decorate(block) {
-  const cols = [...block.firstElementChild.children];
-  const divClass = document.querySelector('.benefit-blocks');
 
-  const innerleftDivs = [...divClass.children].slice(0,1); 
+  const imgIcon = block.querySelector('.button-container');
+  const linkImgicon = document.createElement('img');
+  linkImgicon.src = '/icons/right-arrow.svg';
+  linkImgicon.setAttribute('data-icon-name', 'right-arrow');
+  linkImgicon.className = 'link-img';
+  imgIcon.append(linkImgicon);
+  
+  const innerleftDivs = [...block.children].slice(0,1); 
   const blockleft = document.createElement('div');
   blockleft.classList.add('block-left');
-  divClass.prepend(blockleft);
+  block.prepend(blockleft);
   blockleft.append(innerleftDivs[0]);
-
-  const childLength = [...divClass.children].length;
-  const innerrightDivs = [...divClass.children].slice(1,childLength); 
+  
+  const childLength = [...block.children].length;
+  const innerrightDivs = [...block.children].slice(1,childLength); 
   const blockRight = document.createElement('div');
   blockRight.classList.add('block-right');
-  divClass.append(blockRight);
-  
-  const imgIcon = document.querySelector('.button-container');
-  console.log(imgIcon);
-  const linkImg = document.createElement('img');
-  linkImg.src = '/icons/right-arrow.svg';
-  linkImg.setAttribute('data-icon-name', 'right-arrow');
-  linkImg.className = 'link-img';
-  imgIcon.append(linkImg);
-
-  innerrightDivs.forEach((child) => {
-    blockRight.append(child);
-  });
-
-  [...block.children].forEach((row) => {
-    [...row.children].forEach((col) => {
-      const pic = col.querySelector('picture');
-      if (pic) {
-        const picWrapper = pic.closest('div');
-        if (picWrapper && picWrapper.children.length === 1) {
-          // picture is only content in column
-          picWrapper.classList.add('columns-img-col');
-        }
-      }
-    });
-  });
+  block.append(blockRight);
+  blockRight.append(...innerrightDivs);
 }
