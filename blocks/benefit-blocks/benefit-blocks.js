d69699d5ae44d33e4acf6c5d7e165d82cfa2b11b
Muthu Soundara Pandian <soundhar.sms1992@gmail.com>
2024-11-28 16:29:53 +0530

    es-lint-issue-fixes for benefit block js

--- blocks/benefit-blocks/benefit-blocks.js
+++ blocks/benefit-blocks/benefit-blocks.js
@@ -1,20 +1,20 @@
 export default function decorate(block) {
-
+  // append the img tag for the icon after inner-link
   const imgIcon = block.querySelector('.button-container');
   const linkImgicon = document.createElement('img');
   linkImgicon.src = '/icons/right-arrow.svg';
   linkImgicon.setAttribute('data-icon-name', 'right-arrow');
   linkImgicon.className = 'link-img';
   imgIcon.append(linkImgicon);
-  
-  const innerleftDivs = [...block.children].slice(0,1); 
+  // slicing the first element of the block
+  const innerleftDivs = [...block.children].slice(0, 1);
   const blockleft = document.createElement('div');
   blockleft.classList.add('block-left');
   block.prepend(blockleft);
   blockleft.append(innerleftDivs[0]);
-  
+  // slicing remaining elements of the block
   const childLength = [...block.children].length;
-  const innerrightDivs = [...block.children].slice(1,childLength); 
+  const innerrightDivs = [...block.children].slice(1, childLength);
   const blockRight = document.createElement('div');
   blockRight.classList.add('block-right');
   block.append(blockRight);
