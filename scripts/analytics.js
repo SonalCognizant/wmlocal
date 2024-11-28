8ffdff43524908979fcd9d082ca6a81d6effe4b3
Bhargava <2363065@cognizant.com>
2024-11-28 11:32:44 +0530

    changed dtm

--- scripts/analytics.js
+++ scripts/analytics.js
@@ -22,7 +22,7 @@ function buttonAnalytics() {
             link_domain: linkDomain,
             link_url: linkUrl,
             link_id: linkID || 'undefined',
-            outbound: isOutBound,
+            outbound: String.valueOf(isOutBound),
           },
         });
       });
