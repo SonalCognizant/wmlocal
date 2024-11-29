a33c9f6fa0257e9950e93b4430ee1e443e6b1d0e
Bhargava <2363065@cognizant.com>
2024-11-29 10:52:07 +0530

    changed dtm

--- scripts/analytics.js
+++ scripts/analytics.js
@@ -1,30 +1,77 @@
 /* eslint-env browser */
 
+function callToActionEvent(adobeDataLayer) {
+  adobeDataLayer.push({
+    event: 'call_to_action',
+    eventData: {
+      cta_element_type: '',
+      cta_click_text: '',
+      cta_data_info: '',
+      cta_click_image: '',
+      cta_location: '',
+      cta_type: '',
+    },
+  });
+}
+
+function navigationEvent(adobeDataLayer) {
+  adobeDataLayer.push({
+    event: 'navigation',
+    nav_menu_type: '',
+    nav_click_image_alt_text: '',
+    nav_click_text: '',
+  });
+}
+
+function clickEvent(adobeDataLayer, linkStr, event) {
+  const linkClasses = Array.from(event.currentTarget.classList).join(' ');
+  linkStr.search = '';
+  const linkUrl = linkStr.href;
+  const linkDomain = linkStr.hostname;
+  const linkID = event.currentTarget.getAttribute('id');
+  adobeDataLayer.push({
+    event: 'click',
+    eventData: {
+      link_classes: linkClasses,
+      link_domain: linkDomain,
+      link_url: linkUrl,
+      link_id: linkID || 'undefined',
+      outbound: 'yes',
+    },
+  });
+}
+
+function isOutboundLink(linkURL) {
+  let outboundLink = false;
+  if (!linkURL.hostname) {
+    outboundLink = false;
+  }
+  if (linkURL.hostname !== window.location.hostname) {
+    outboundLink = true;
+  }
+  return outboundLink;
+}
+
 function buttonAnalytics(adobeDataLayer) {
-  const buttons = document.querySelectorAll('.button-container a.button');
+  const buttons = document.querySelectorAll('a');
   buttons.forEach((button) => {
     button.addEventListener('click', (event) => {
-      const linkClasses = Array.from(event.currentTarget.classList).join(' ');
-
       const linkHref = event.currentTarget.getAttribute('href');
-      const linkStr = new URL(linkHref);
-      linkStr.search = '';
-      const linkUrl = linkStr.href;
-      const linkDomain = linkStr.hostname;
-      const linkID = event.currentTarget.getAttribute('id');
+      const linkURL = new URL(linkHref);
+      if (linkHref) {
+        if (isOutboundLink(linkURL)) {
+          clickEvent(adobeDataLayer, linkURL, event);
+        }
 
-      const isOutBound = linkDomain !== window.location.hostname;
-      const isOutBoundString = String(isOutBound);
-      adobeDataLayer.push({
-        event: 'click',
-        eventData: {
-          link_classes: linkClasses,
-          link_domain: linkDomain,
-          link_url: linkUrl,
-          link_id: linkID || 'undefined',
-          outbound: isOutBoundString,
-        },
-      });
+        if (button.classList.contains('button')) {
+          callToActionEvent(adobeDataLayer);
+        } else if (
+          !button.classList.contains('button') &&
+          !isOutboundLink(linkURL)
+        ) {
+          navigationEvent(adobeDataLayer);
+        }
+      }
     });
   });
 }
