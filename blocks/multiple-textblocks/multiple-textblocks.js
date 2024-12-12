de02549e50fc1276b681035dae3dc9ce1b06c839
Anubhav Tyagi <anubhav.16bcs1079@abes.ac.in>
2024-12-12 12:00:03 +0530

    Adding multiple-textblocks files

--- blocks/multiple-textblocks/multiple-textblocks.js
+++ blocks/multiple-textblocks/multiple-textblocks.js
@@ -4,16 +4,15 @@ export default function decorate(block) {
   const imageWrapperDiv = block.parentElement;
   const mainHeading = document.createElement('h2');
   mainHeading.textContent = `${title}`;
+  mainHeading.classList.add('title');
   imageWrapperDiv.prepend(mainHeading);
-  console.log(block, title, mainHeading, imageWrapperDiv);
-  console.log(block.children);
   [...block.children].forEach((row, index) => {
     const li = document.createElement('li');
     if (index > 0) {
       while (row.firstElementChild) {
         li.append(row.firstElementChild);
         [...li.children].forEach((div) => {
-          div.className = 'cards-card-body';
+          div.className = 'block-div';
         });
         ul.append(li);
       }
