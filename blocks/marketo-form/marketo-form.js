b8c93a24ac109ad77749c81692590ad316767682
Muthu Soundara Pandian <soundhar.sms1992@gmail.com>
2024-12-16 17:12:30 +0530

    commit for es-lint issues

--- blocks/marketo-form/marketo-form.js
+++ blocks/marketo-form/marketo-form.js
@@ -3,19 +3,22 @@ function addScript(src) {
   script.src = src;
   document.head.appendChild(script);
 }
+(() => {
+  addScript('//899-BTB-436.mktoweb.com/js/forms2/js/forms2.min.js');
+})();
 // creation of block for marketo form
 export default function decorate(block) {
-  addScript('//899-BTB-436.mktoweb.com/js/forms2/js/forms2.min.js');
   [...block.children].forEach((row) => {
     const section = row.querySelector('p');
     const divId = section.innerHTML;
     const idstringValue = divId.split('_')[1];
     const cForm = document.createElement('form');
+    cForm.classList.add('eds-mktoForm');
     cForm.setAttribute('id', divId);
     section.insertAdjacentElement('afterend', cForm);
     setTimeout(() => {
       const formId = parseInt(idstringValue, 10);
-      MktoForms2.loadForm('//899-BTB-436.mktoweb.com', '899-BTB-436', formId);
+      window.MktoForms2.loadForm('//899-BTB-436.mktoweb.com', '899-BTB-436', formId);
     }, 1000);
   });
 }
