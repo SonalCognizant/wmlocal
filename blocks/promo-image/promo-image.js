536dde6c0e09292ee6b5c00d61b7f8ba8e405f4c
Anubhav Tyagi <anubhav.16bcs1079@abes.ac.in>
2024-12-17 12:55:06 +0530

    configuration for font awesome

--- blocks/promo-image/promo-image.js
+++ blocks/promo-image/promo-image.js
@@ -10,13 +10,11 @@ export default function decorate(block) {
     180deg, 
     rgb(75, 76, 77) 0%, 
     rgba(93, 93, 93, 0.65) 29%, 
-    rgba(115, 115, 115, 0.2)
+    rgba(115, 115, 115, 0.2) 100%,
   ), url(${backgroundImage})`;
   imageWrapperDiv.style.backgroundSize = 'cover';
   imageWrapperDiv.style.backgroundPosition = 'center';
   imageWrapperDiv.style.backgroundRepeat = 'no-repeat';
-  imageWrapperDiv.style.height = '600px';
-  imageWrapperDiv.style.width = '100%';
   const mainHeading = document.createElement('h2');
   mainHeading.classList.add('promo-image-heading');
   mainHeading.textContent = `${title}`;
