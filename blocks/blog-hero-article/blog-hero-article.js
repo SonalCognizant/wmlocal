2cc645b5ebbd90a0f06c8033b344f64a20e0ce09
Anubhav Tyagi <anubhav.16bcs1079@abes.ac.in>
2025-01-06 16:47:45 +0530

    blog-hero-article block

--- blocks/blog-hero-article/blog-hero-article.js
+++ blocks/blog-hero-article/blog-hero-article.js
@@ -1,18 +1,36 @@
-export default function decorate(block) {
-  const cols = [...block.firstElementChild.children];
-  block.classList.add(`columns-${cols.length}-cols`);
+// export default function decorate(block) {
+//   const cols = [...block.firstElementChild.children];
+//   block.classList.add(`columns-${cols.length}-cols`);
 
-  // setup image columns
-  [...block.children].forEach((row) => {
-    [...row.children].forEach((col) => {
-      const pic = col.querySelector('picture');
-      if (pic) {
-        const picWrapper = pic.closest('div');
-        if (picWrapper && picWrapper.children.length === 1) {
-          // picture is only content in column
-          picWrapper.classList.add('columns-img-col');
-        }
-      }
-    });
-  });
+//   // setup image columns
+//   [...block.children].forEach((row) => {
+//     [...row.children].forEach((col) => {
+//       const pic = col.querySelector('picture');
+//       if (pic) {
+//         const picWrapper = pic.closest('div');
+//         if (picWrapper && picWrapper.children.length === 1) {
+//           // picture is only content in column
+//           picWrapper.classList.add('columns-img-col');
+//         }
+//       }
+//     });
+//   });
+// }
+import { getMetadata } from '../../scripts/aem.js';
+import { loadFragment } from '../fragment/fragment.js';
+
+export default async function decorate(block) {
+  // load footer as fragment
+  const blogHeroMeta = getMetadata('blog-hero');
+  console.log(blogHeroMeta);
+  const blogHeroPath = blogHeroMeta
+    ? new URL(blogHeroMeta, window.location).pathname
+    : '/blog-hero';
+  const fragment = await loadFragment(blogHeroPath);
+  console.log(blogHeroPath);
+  console.log(fragment);
+  // decorate footer DOM
+  block.textContent = '';
+  const blogHero = document.createElement('div');
+  block.append(blogHero);
 }
