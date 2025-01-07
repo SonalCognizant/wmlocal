deddd6cc5f2ed0a75d2e20819329bac846eb73f0
Muthu Soundara Pandian <soundhar.sms1992@gmail.com>
2025-01-07 16:24:58 +0530

    article-information block

---
+++ blocks/blog-article-information/blog-article-information.js
@@ -0,0 +1,24 @@
+export default function decorate(block) {
+  const blockChild = block.children[0].children[0];
+  const dates = block.children[0].children[0].querySelector('p');
+  if (dates) {
+    dates.parentElement.classList.add('date');
+  }
+  const date = block.children[0].children[1].querySelector('p');
+  if (date) {
+    blockChild.append(date);
+  }
+  const author = block.children[0].children[2].querySelector('p');
+  if (author) {
+    author.parentElement.classList.add('author');
+  }
+  const linkParagraphs = block.children[1].children[0].querySelectorAll('p');
+  linkParagraphs.forEach((p) => {
+    p.parentElement.classList.add('article-link');
+  });
+  const audio = block.children[2].children[0].querySelector('p');
+  if (audio) {
+    audio.parentElement.classList.add('article-audio');
+  }
+}
+  
