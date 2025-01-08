5af6a3b0e019fe190d93d877ad4e425de3d6767e
Bhargava <2363065@cognizant.com>
2025-01-08 11:18:42 +0530

    changes

--- scripts/aem.js
+++ scripts/aem.js
@@ -13,16 +13,18 @@
 /* eslint-env browser */
 function sampleRUM(checkpoint, data) {
   // eslint-disable-next-line max-len
-  const timeShift = () => (window.performance
-    ? window.performance.now()
-    : Date.now() - window.hlx.rum.firstReadTime);
+  const timeShift = () =>
+    window.performance
+      ? window.performance.now()
+      : Date.now() - window.hlx.rum.firstReadTime;
   try {
     window.hlx = window.hlx || {};
     sampleRUM.enhance = () => {};
     if (!window.hlx.rum) {
-      const weight = new URLSearchParams(window.location.search).get('rum') === 'on'
-        ? 1
-        : 100;
+      const weight =
+        new URLSearchParams(window.location.search).get('rum') === 'on'
+          ? 1
+          : 100;
       const id = Math.random().toString(36).slice(-4);
       const isSelected = Math.random() * weight < 1;
       // eslint-disable-next-line object-curly-newline, max-len
@@ -55,9 +57,11 @@ function sampleRUM(checkpoint, data) {
             sampleRUM('error', errData);
           });
         });
-        sampleRUM.baseURL = sampleRUM.baseURL
-          || new URL(window.RUM_BASE || '/', new URL('https://rum.hlx.page'));
-        sampleRUM.collectBaseURL = sampleRUM.collectBaseURL || sampleRUM.baseURL;
+        sampleRUM.baseURL =
+          sampleRUM.baseURL ||
+          new URL(window.RUM_BASE || '/', new URL('https://rum.hlx.page'));
+        sampleRUM.collectBaseURL =
+          sampleRUM.collectBaseURL || sampleRUM.baseURL;
         sampleRUM.sendPing = (ck, time, pingData = {}) => {
           // eslint-disable-next-line max-len, object-curly-newline
           const rumData = JSON.stringify({
@@ -70,11 +74,12 @@ function sampleRUM(checkpoint, data) {
           });
           const { href: url, origin } = new URL(
             `.rum/${weight}`,
-            sampleRUM.collectBaseURL,
+            sampleRUM.collectBaseURL
           );
-          const body = origin === window.location.origin
-            ? new Blob([rumData], { type: 'application/json' })
-            : rumData;
+          const body =
+            origin === window.location.origin
+              ? new Blob([rumData], { type: 'application/json' })
+              : rumData;
           navigator.sendBeacon(url, body);
           // eslint-disable-next-line no-console
           console.debug(`ping:${ck}`, pingData);
@@ -85,7 +90,7 @@ function sampleRUM(checkpoint, data) {
           const script = document.createElement('script');
           script.src = new URL(
             '.rum/@adobe/helix-rum-enhancer@^2/src/index.js',
-            sampleRUM.baseURL,
+            sampleRUM.baseURL
           ).href;
           document.head.appendChild(script);
         };
@@ -98,7 +103,7 @@ function sampleRUM(checkpoint, data) {
       window.hlx.rum.collector(checkpoint, data, timeShift());
     }
     document.dispatchEvent(
-      new CustomEvent('rum', { detail: { checkpoint, data } }),
+      new CustomEvent('rum', { detail: { checkpoint, data } })
     );
   } catch (error) {
     // something went wrong
@@ -113,13 +118,14 @@ function setup() {
   window.hlx.RUM_MASK_URL = 'full';
   window.hlx.RUM_MANUAL_ENHANCE = true;
   window.hlx.codeBasePath = '';
-  window.hlx.lighthouse = new URLSearchParams(window.location.search).get('lighthouse') === 'on';
+  window.hlx.lighthouse =
+    new URLSearchParams(window.location.search).get('lighthouse') === 'on';
 
   const scriptEl = document.querySelector('script[src$="/scripts/scripts.js"]');
   if (scriptEl) {
     try {
       [window.hlx.codeBasePath] = new URL(scriptEl.src).pathname.split(
-        '/scripts/scripts.js',
+        '/scripts/scripts.js'
       );
     } catch (error) {
       // eslint-disable-next-line no-console
@@ -145,10 +151,10 @@ function init() {
 function toClassName(name) {
   return typeof name === 'string'
     ? name
-      .toLowerCase()
-      .replace(/[^0-9a-z]/gi, '-')
-      .replace(/-+/g, '-')
-      .replace(/^-|-$/g, '')
+        .toLowerCase()
+        .replace(/[^0-9a-z]/gi, '-')
+        .replace(/-+/g, '-')
+        .replace(/^-|-$/g, '')
     : '';
 }
 
@@ -278,7 +284,7 @@ function createOptimizedPicture(
   breakpoints = [
     { media: '(min-width: 600px)', width: '2000' },
     { width: '750' },
-  ],
+  ]
 ) {
   const url = new URL(src, window.location.href);
   const picture = document.createElement('picture');
@@ -292,7 +298,7 @@ function createOptimizedPicture(
     source.setAttribute('type', 'image/webp');
     source.setAttribute(
       'srcset',
-      `${pathname}?width=${br.width}&format=webply&optimize=medium`,
+      `${pathname}?width=${br.width}&format=webply&optimize=medium`
     );
     picture.appendChild(source);
   });
@@ -304,7 +310,7 @@ function createOptimizedPicture(
       if (br.media) source.setAttribute('media', br.media);
       source.setAttribute(
         'srcset',
-        `${pathname}?width=${br.width}&format=${ext}&optimize=medium`,
+        `${pathname}?width=${br.width}&format=${ext}&optimize=medium`
       );
       picture.appendChild(source);
     } else {
@@ -314,7 +320,7 @@ function createOptimizedPicture(
       picture.appendChild(img);
       img.setAttribute(
         'src',
-        `${pathname}?width=${br.width}&format=${ext}&optimize=medium`,
+        `${pathname}?width=${br.width}&format=${ext}&optimize=medium`
       );
     }
   });
@@ -365,15 +371,16 @@ function wrapTextNodes(block) {
 
   block.querySelectorAll(':scope > div > div').forEach((blockColumn) => {
     if (blockColumn.hasChildNodes()) {
-      const hasWrapper = !!blockColumn.firstElementChild
-        && validWrappers.some(
-          (tagName) => blockColumn.firstElementChild.tagName === tagName,
+      const hasWrapper =
+        !!blockColumn.firstElementChild &&
+        validWrappers.some(
+          (tagName) => blockColumn.firstElementChild.tagName === tagName
         );
       if (!hasWrapper) {
         wrap(blockColumn);
       } else if (
-        blockColumn.firstElementChild.tagName === 'PICTURE'
-        && (blockColumn.children.length > 1 || !!blockColumn.textContent.trim())
+        blockColumn.firstElementChild.tagName === 'PICTURE' &&
+        (blockColumn.children.length > 1 || !!blockColumn.textContent.trim())
       ) {
         wrap(blockColumn);
       }
@@ -393,25 +400,25 @@ function decorateButtons(element) {
       const twoup = a.parentElement.parentElement;
       if (!a.querySelector('img')) {
         if (
-          up.childNodes.length === 1
-          && (up.tagName === 'P' || up.tagName === 'DIV')
+          up.childNodes.length === 1 &&
+          (up.tagName === 'P' || up.tagName === 'DIV')
         ) {
           up.classList.add('button-container');
         }
         if (
-          up.childNodes.length === 1
-          && up.tagName === 'STRONG'
-          && twoup.childNodes.length === 1
-          && twoup.tagName === 'P'
+          up.childNodes.length === 1 &&
+          up.tagName === 'STRONG' &&
+          twoup.childNodes.length === 1 &&
+          twoup.tagName === 'P'
         ) {
           a.className = 'button primary';
           twoup.classList.add('button-container');
         }
         if (
-          up.childNodes.length === 1
-          && up.tagName === 'EM'
-          && twoup.childNodes.length === 1
-          && twoup.tagName === 'P'
+          up.childNodes.length === 1 &&
+          up.tagName === 'EM' &&
+          twoup.childNodes.length === 1 &&
+          twoup.tagName === 'P'
         ) {
           a.className = 'button secondary';
           twoup.classList.add('button-container');
@@ -597,7 +604,7 @@ async function loadBlock(block) {
     const { blockName } = block.dataset;
     try {
       const cssLoaded = loadCSS(
-        `${window.hlx.codeBasePath}/blocks/${blockName}/${blockName}.css`,
+        `${window.hlx.codeBasePath}/blocks/${blockName}/${blockName}.css`
       );
       const decorationComplete = new Promise((resolve) => {
         (async () => {
@@ -726,6 +733,7 @@ async function loadSections(element) {
 }
 
 function loadPublishedDate() {
+  const currPagePath = window.location.pathname;
   const sitemapjsonUrl = '/sitemap.json';
   fetch(sitemapjsonUrl)
     .then((response) => {
@@ -735,15 +743,16 @@ function loadPublishedDate() {
       return response.json();
     })
     .then((data) => {
-      const result = data.data; // Assuming the JSON has a `data` property
-      console.log(result);
+      const found = data.data.find((item) => item.path === currPagePath);
+      if (found) {
+        console.log(found.lastModified, found.lastPublished);
+      }
     })
     .catch((error) => {
       // eslint-disable-next-line no-console
       console.error('There was a problem with the fetch operation:', error);
     });
 }
-
 
 init();
 
