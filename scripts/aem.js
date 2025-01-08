e88985eb49e09ad32b6dad3dd3b385734e6aa1f4
Bhargava <2363065@cognizant.com>
2025-01-08 12:01:11 +0530

    changes

--- scripts/aem.js
+++ scripts/aem.js
@@ -735,7 +735,7 @@ async function loadSections(element) {
 function formatDate(timestamp) {
   const date = new Date(timestamp * 1000);
   const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
   const year = date.getFullYear();
   return `${month}/${day}/${year}`;
 }
