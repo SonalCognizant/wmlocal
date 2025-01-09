7874957a6576650ea9e277ca4956c6f454d6bde6
Anubhav Tyagi <anubhav.16bcs1079@abes.ac.in>
2025-01-09 16:19:53 +0530

    blog-hero-article block

--- blocks/blog-hero-article/blog-hero-article.js
+++ blocks/blog-hero-article/blog-hero-article.js
@@ -12,7 +12,7 @@ export default async function decorate(block) {
   const mainHeading = fragment.children[0].textContent;
   const datafromArticleInformation = fragment.children[1].children[0];
   const datafromImageContent = fragment.children[1].children[1];
-  let lastUpdatedDate = datafromArticleInformation.querySelector('.date');
+  const lastUpdatedDate = datafromArticleInformation.querySelector('.date p span');
   const articleReadTime = datafromArticleInformation.querySelector('.article-link').children[1];
   const imageSrc = datafromImageContent.querySelector('.columns-img-col p picture').innerHTML;
   const description = datafromImageContent.querySelector('.image-text').children[0].children[0].textContent;
@@ -29,11 +29,12 @@ export default async function decorate(block) {
   descriptionPara.append(description);
   descriptionDiv.append(descriptionPara);
   const lastUpdatedpara = document.createElement('p');
-  if (lastUpdatedDate.children[1] !== 'undefined' && lastUpdatedDate !== 'undefined') {
-    lastUpdatedpara.append(lastUpdatedDate?.children[1]?.innerText);
-  } else {
-    lastUpdatedDate = 'MM/DD/YYYY';
-  }
+  lastUpdatedpara.append(lastUpdatedDate.innerText);
+  // if (lastUpdatedDate.children[1] !== 'undefined' && lastUpdatedDate !== 'undefined') {
+  //   lastUpdatedpara.append(lastUpdatedDate?.children[1]?.innerText);
+  // } else {
+  //   lastUpdatedDate = 'MM/DD/YYYY';
+  // }
   categoryDateDiv.append(lastUpdatedpara, articleReadTime);
   contentDiv.append(heading, categoryDateDiv, descriptionDiv);
   blogHero.append(imgDiv, contentDiv);
