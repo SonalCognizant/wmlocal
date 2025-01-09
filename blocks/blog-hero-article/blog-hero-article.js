de8337f1d14683e2be598aa6f5b113a832a12274
Anubhav Tyagi <anubhav.16bcs1079@abes.ac.in>
2025-01-09 15:25:19 +0530

    blog-hero-article block

--- blocks/blog-hero-article/blog-hero-article.js
+++ blocks/blog-hero-article/blog-hero-article.js
@@ -2,30 +2,22 @@ import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
 
 export default async function decorate(block) {
-  // load footer as fragment
-  // console.log(block);
   const blogHeroMeta = getMetadata('blog-hero');
   const blogHeroPath = blogHeroMeta
     ? new URL(blogHeroMeta, window.location).pathname
     : '/blog-hero';
   const fragment = await loadFragment(blogHeroPath);
-  // console.log(fragment);
   const mainHeading = fragment.children[0].textContent;
   const datafromArticleInformation = fragment.children[1].children[0];
   const datafromImageContent = fragment.children[1].children[1];
   const lastUpdatedDate = datafromArticleInformation.querySelector('.date');
-  const articleReadTime = datafromArticleInformation.querySelector('.article-link').textContent;
+  // const articleReadTime = datafromArticleInformation.querySelector('.article-link').textContent;
   const imageSrc = datafromImageContent.querySelector('.columns-img-col p picture').innerHTML;
   const description = datafromImageContent.querySelector('.image-text').children[0].children[0].textContent;
-  // console.log(lastUpdatedDate.lastChild.textContent, 'lastUpdatedDate');
-  // console.log(articleReadTime, 'articleReadTime');
-  // console.log(anchorscatoegory, 'acnhors');
-  // console.log(imageSrc, description);
   block.textContent = '';
   const blogHero = document.createElement('div');
   const imgDiv = document.createElement('div');
   const contentDiv = document.createElement('div');
-  // imgDiv.append(imageSrc);
   imgDiv.innerHTML = imageSrc;
   const categoryDateDiv = document.createElement('div');
   const descriptionDiv = document.createElement('div');
@@ -34,7 +26,7 @@ export default async function decorate(block) {
   heading.append(mainHeading);
   descriptionPara.append(description);
   descriptionDiv.append(descriptionPara);
-  categoryDateDiv.append(lastUpdatedDate, articleReadTime);
+  categoryDateDiv.append(lastUpdatedDate.children[1].textContent);
   contentDiv.append(heading, categoryDateDiv, descriptionDiv);
   blogHero.append(imgDiv, contentDiv);
   block.append(blogHero);
