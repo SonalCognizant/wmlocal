f665bc7e19f0046680b41fc250044b8d374e06d5
Muthu Soundara Pandian <soundhar.sms1992@gmail.com>
2025-01-10 12:20:09 +0530

    benefit-block icon

--- blocks/benefit-block-icon/benefit-block-icon.js
+++ blocks/benefit-block-icon/benefit-block-icon.js
@@ -20,6 +20,17 @@ export default function decorate(block) {
   block.append(blockRight);
   if (innerrightDivs) {
     blockRight.append(...innerrightDivs);
+    // Change all the second <h2> to <h4>
+    const divs = blockRight.querySelectorAll('div');
+    divs.forEach((div) => {
+      const h2s = div.querySelectorAll('h2');
+      if (h2s.length > 1) {
+        const secondH2 = h2s[1];
+        const h4 = document.createElement('h4');
+        h4.innerHTML = secondH2.innerHTML; // Copy the inner HTML
+        secondH2.replaceWith(h4); // Replace the <h2> with <h4>
+      }
+    });
     // Remove empty divs
     const emptyDivs = blockRight.querySelectorAll('div:empty');
     emptyDivs.forEach((div) => div.remove());
