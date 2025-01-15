9dbc79ddf81667b520763a2cc7586f3be7b09a0d
Anubhav Tyagi <anubhav.16bcs1079@abes.ac.in>
2025-01-15 13:20:03 +0530

    defect promo text lg icon

--- blocks/blog-hero-article/blog-hero-article.js
+++ blocks/blog-hero-article/blog-hero-article.js
@@ -10,7 +10,7 @@ export default async function decorate(block) {
   const blogHeroPath = block.children[0].innerText;
   console.log(blogHeroPath);
   block.innerHTML = '';
-  const fragment = await loadFragment(blogHeroPath);
+  const fragment = loadFragment(blogHeroPath);
   console.log(fragment);
   const mainHeading = fragment.children[0].textContent;
   const datafromArticleInformation = fragment.children[1].children[0];
